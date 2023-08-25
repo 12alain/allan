859e6590de85267157cc61bf7165cf0a2f553560
12alain <kiemdealain02@gmail.com>
2023-08-25 13:30:13 +0000

    Create_project_structure . closes #1

---
+++ application.py
@@ -0,0 +1,16 @@
+import os
+def create_project_structure():
+
+    directory = os.path.join(os.getcwd())
+    folders = ["data/raw", "data/cleaned", "docs",
+               "models", "notebooks", "reports", "src"]
+
+
+    for folder in folders:
+            
+            folder_path = os.path.join(directory, folder)
+            os.makedirs(folder_path, exist_ok=True)
+            # Crée un fichier .gitkeep dans les dossiers vides pour qu'ils soient suivis
+            with open(os.path.join(folder_path, ".gitkeep"), 'w') as f:
+                pass
+create_project_structure()
